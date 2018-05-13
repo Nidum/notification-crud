5fbac1d206d21133672c73ab6b16784202dae461
Nidum <lalaith.ned.midh@gmail.com>
2018-05-13 21:21:55 +0300

    Add example unit tests

---
+++ src/test/java/sticker/ExampleTest.java
@@ -0,0 +1,23 @@
+package sticker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Emiliia Nesterovych on 5/13/2018.
+ */
+
+public class ExampleTest {
+    @Test
+    public void exampleTrue(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void exampleFalse(){
+        assertFalse(false);
+    }
+
+}
